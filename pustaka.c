3cf1c2ec0907624826a265bcb14c9321ae7c79ea
rizkimufti <bayugrafit@gmail.com>
2018-05-28 13:29:43 +0700

    edited function

--- pustaka.c
+++ pustaka.c
@@ -1,6 +1,6 @@
 #include "pustaka.h"
 
-void menu(){
+void menu(){ ///  ==> fungsi ini berguna untuk menampilkan menu yan gada
     puts("====== Kalender Kegiatan ======");
     puts("1. Tambah Kegiatan");
     puts("2. Tampilkan Kegiatan");
@@ -9,25 +9,35 @@ void menu(){
     puts("5. Urutkan Kegiatan");
 }
 
-kalender makeKegiatan(char kegiatan[], int tanggal, int waktu, char nama[]){
-    kalender baru;
-    strcpy(baru.kegiatan, kegiatan);
-    strcpy(baru.nama, nama);
-    baru.tanggal = tanggal;
-    baru.waktu = waktu;
+void inputKegiatan(){ /// ==> fungsi ini berguna untuk menginputkand data atau menambahkan data yang ada kepada client
+    int i=0;
+    printf("Masukan banyak data : "); scanf("%d", &batas);
 
-    return baru;
+    while(i<batas){
+        printf("No. %d\n", i+1);
+        printf("Masukan tanggal kegiatan (dd-mm-yy) : ");
+        scanf("%d-%d-%d", &data[i].tgl.d, &data[i].tgl.m, &data[i].tgl.y);
+
+        puts("");
+        printf("Masukkan waktu kegiatan (mm:hh) : ");
+        scanf("%d-%d", &data[i].wak.m, &data[i].wak.h);
+
+        puts("");
+        __fpurge(stdin);
+        printf("Masukkan Nama Kegiatan : "); gets(data[i].kegiatan);
+        printf("Masukkan Nama Terkait : "); gets(data[i].nama);
+        i++;
+    }
 }
 
-void inputKegiatan(){
-    kalender P;
-    printf("Masukan Namak Kegiatan = "); gets(P.kegiatan);
-    printf("Masukkan nama terkait = "); gets(P.nama);
-    printf("Masukan tanggal kegiatan = "); scanf("%d", &P.tanggal);
-    printf("Masukkan Waktu Kegiatan = "); scanf("%d", &P.waktu);
-}
-
-void showKegiatan(){
-    kalender P;
-    printf("%d %s %d %s", P.tanggal, P.kegiatan, P.waktu, P.nama);
+void showKegiatan(){ /// ==> fungsi ini berguna untuk menampilkan data yang ada pada ADT kalender
+    int i=0;
+    while(i<batas){
+        printf("No. %d\n", i+1);
+        printf("Tanggal Kegiatan : %d-%d-%d\n", data[i].tgl.d, data[i].tgl.m, data[i].tgl.y);
+        printf("Nama Kegiatan : %s\n", data[i].kegiatan);
+        printf("Orang Terkait : %s\n", data[i].nama);
+        printf("Waktu Kegiatan : %d:%d", data[i].wak.m, data[i].wak.h);
+        i++;
+    }
 }
