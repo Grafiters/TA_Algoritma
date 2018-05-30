fa1d907ae6de4a654fa8f161f71d0dbb5f56d72d
rizkimufti <bayugrafit@gmail.com>
2018-05-30 23:45:56 +0700

    edited function

--- pustaka.c
+++ pustaka.c
@@ -17,12 +17,30 @@ void inputKegiatan(){ /// ==> fungsi ini berguna untuk menginputkand data atau m
         printf("No. %d\n", i+1);
         printf("Masukan tanggal kegiatan (dd-mm-yy) : ");
         scanf("%d-%d-%d", &data[i].tgl.d, &data[i].tgl.m, &data[i].tgl.y);
+        if(data[i].tgl.d>28 && data[i].tgl.m==2 && data[i].tgl.y%4 != 0){
+            printf("format anda salah silahkan masukkan kembali = ");
+            scanf("%d-%d-%d", &data[i].tgl.d, &data[i].tgl.m, &data[i].tgl.y);
+        }else if(data[i].tgl.d>31){
+            printf("format anda salah silahkan masukkan kembali = ");
+            scanf("%d-%d-%d", &data[i].tgl.d, &data[i].tgl.m, &data[i].tgl.y);
+        }else if(data[i].tgl.m>12){
+            printf("format anda salah silahkan masukkan kembali = ");
+            scanf("%d-%d-%d", &data[i].tgl.d, &data[i].tgl.m, &data[i].tgl.y);
+        }
 
         puts("");
-        printf("Masukkan waktu kegiatan (mm:hh) : ");
-        scanf("%d-%d", &data[i].wak.m, &data[i].wak.h);
+        printf("Masukkan waktu kegiatan (hh:mm) : ");
+        scanf("%d-%d", &data[i].wak.h, &data[i].wak.m);
+        if(data[i].wak.h>23){
+            printf("format anda salah silahkan masukkan kembali = ");
+            scanf("%d-%d", &data[i].wak.h, &data[i].wak.m);
+        }else if(data[i].wak.m>59){
+            printf("format anda salah silahkan masukkan kembali = ");
+            scanf("%d-%d", &data[i].wak.h, &data[i].wak.m);
+        }
 
         puts("");
+        fflush(stdin);
         __fpurge(stdin);
         printf("Masukkan Nama Kegiatan : "); gets(data[i].kegiatan);
         printf("Masukkan Nama Terkait : "); gets(data[i].nama);
@@ -39,5 +57,30 @@ void showKegiatan(){ /// ==> fungsi ini berguna untuk menampilkan data yang ada 
         printf("Orang Terkait : %s\n", data[i].nama);
         printf("Waktu Kegiatan : %d:%d", data[i].wak.m, data[i].wak.h);
         i++;
+    }
+}
+
+int tukar(int *num1, int *num2){
+    int temp = *num1;
+        *num1 = *num2;
+        *num2 = temp;
+}
+
+int inner_rec(int data[], int cur, int arrSize){
+    if(data[arrSize-1]<data[cur]){
+        return cur=arrSize;
+    }else{
+        return inner_rec(data,cur,arrSize-1);
+    }
+}
+
+void urutKegiatan(int batas){
+    if(batas==0){
+        int post=inner_rec(data,batas-1,batas);
+        tukar(&data[post],&data[batas-1]);
+    }else{
+        int post=inner_rec(data,batas-1,batas);
+        tukar(&data[post],&data[batas-1]);
+        urutKegiatan(batas-1);
     }
 }
