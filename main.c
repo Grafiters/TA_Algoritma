d4d9bc09d5ff19a0517a99e77bbfa2b4bcf35612
rizkimufti <bayugrafit@gmail.com>
2018-06-04 18:38:29 +0700

    edited main

--- main.c
+++ main.c
@@ -1,21 +1,23 @@
 #include "pustaka.h"
 
-int jumlah;
-int sampai, limit=0;
+int jumlah, hapus, b, p, w;
+int sampai, limit=0, pilih;
+char nama[50];
 
 int main()
 {
     kalender acara[100];
     daftar:
+    system("cls");
     menu();
 
-    int pilih;
     printf("Silahkan pilih Menu = "); scanf("%d", &pilih);
 
+//    __fpurge(stdin);
     fflush(stdin);
     switch(pilih){
         case 1:
-            system("clear");
+            system("cls");
             printf("Masukan jumlah kegiatan = "); scanf("%d", &jumlah);
             sampai = jumlah + limit;
             int j;
@@ -26,18 +28,17 @@ int main()
                 inputKegiatan(&acara[j]);
             }
             limit += jumlah;
-            system("clear");
+            system("cls");
             goto daftar;
             break;
         case 2:
+            system("cls");
             printf("1. lihat acara\n");
             printf("2. lihat details acara\n");
-            int p;
             printf("pilih melihat kegiatan = "); scanf("%d", &p);
             if(p==1){
-                __fpurge(stdin);
+//                __fpurge(stdin);
                 fflush(stdin);
-                system("clear");
                 int i, k;
                 for(k=0;k<sampai;k++){
                     printf("No. %d\n", k+1);
@@ -51,8 +52,8 @@ int main()
                 }
             }else if(p==2){
                 int i, k;
-                system("clear");
-                __fpurge(stdin);
+                system("cls");
+//                __fpurge(stdin);
                 fflush(stdin);
                 for(k=0;k<sampai;k++){
                     printf("No. %d\n", k+1);
@@ -70,24 +71,68 @@ int main()
             }
             break;
         case 3:
+            system("cls");
+//            __fpurge(stdin);
+            fflush(stdin);
+            int c;
+            for(c=0;c<limit;c++){
+                printf("No. %d\n", c+1);
+                showKegiatan(acara[c]);
+            }
+            printf("Masukan data yang ingin anda hapus = "); scanf("%d", &hapus);
+
+//            __fpurge(stdin);
+            fflush(stdin);
+
+            hapus -= 1;
+
+            showKegiatan(acara[hapus]);
+
+            char yakin;
+            printf("apakah anda yakin ingin menghapus data ini (y/n ) = "); scanf("%c", &yakin);
+                if(yakin == 'y'){
+                    hapuskegiatan(acara,limit);
+                }
+            limit --;
+            goto daftar;
             break;
         case 4:
+            system("cls");
+            int y;
+            printf("Masukkan nama yang akan dicari : "); gets(nama);
+
+            cariKegiatan(acara,nama,limit);
+
+            printf("0 kembali ke menu = "); scanf("%d", &y);
+            if (y==0)
+            {
+                goto daftar;
+            }
             break;
         case 5:
+            system("cls");
+            int f;
+            puts("Urut data \n");
             urutKegiatan(acara,limit);
-            int i;
-            printf("klik 0 untuk kembali ke menu utama = "); scanf("%d", &i);
-                if(i==0){
+            puts("");
+            for (w=0;w<limit;w++)
+            {
+                showKegiatan(acara[w]);
+                printf("ketik 0 untuk kembali ke menu : "); scanf("%d", &f);
+                if(f==0){
                     goto daftar;
                 }else{
+                    printf("good bye");
                     return 0;
                 }
+            }
             break;
         case 6:
+            system("cls");
             credits();
-            int i;
-            printf("klik 0 untuk kembali ke menu utama = "); scanf("%d", &i);
-                if(i==0){
+            int a;
+            printf("klik 0 untuk kembali ke menu utama = "); scanf("%d", &a);
+                if(a==0){
                     goto daftar;
                 }else{
                     return 0;
@@ -95,7 +140,7 @@ int main()
             break;
         default:
             printf("= Maaf Anda Kurang Beruntung = \n");
-            __fpurge(stdin);
+//            __fpurge(stdin);
             fflush(stdin);
     }
     return 0;
